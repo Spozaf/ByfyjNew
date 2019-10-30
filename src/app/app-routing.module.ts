232be656147bac2c8daf0a517cdee57b3d08e69f
unknown <spozaf.kontakt@gmail.com>
2019-10-30 14:53:14 +0100

    cookies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { KontaktComponent } from './kontakt/kontakt.component';
 import { OnasComponent } from './onas/onas.component';
+import { CiasteczkaComponent } from './ciasteczka/ciasteczka.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   {path: 'menu', component: MenuComponent},
   {path: 'kontakt', component: KontaktComponent},
-  {path: 'onas', component: OnasComponent}
+  {path: 'onas', component: OnasComponent},
+  {path: 'ciasteczka', component: CiasteczkaComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,33 @@ import { PopUpModule } from './shared-components/popup/popup.module';
 import { OnasComponent } from './onas/onas.component';
 
 import { LayoutModule } from '@angular/cdk/layout';
+import { CiasteczkaComponent } from './ciasteczka/ciasteczka.component';
+
+import {NgcCookieConsentModule, NgcCookieConsentConfig} from 'ngx-cookieconsent';
+
+const cookieConfig:NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'localhost' // or 'your.domain.com' // it is mandatory to set a domain, for cookies to work properly (see https://goo.gl/S2Hy2A)
+  },
+  palette: {
+    popup: {
+      background: '#844625'
+    },
+    button: {
+      background: '#DDC391'
+    }
+  },
+  theme: 'edgeless',
+  type: 'info',
+  "content": {
+    'message': 'Ta strona używa plików cookie.',
+    'dismiss': 'Lubie ciastka!',
+    'deny': '',
+    'link': 'Dowiedz się więcej',
+    'href': '/ciasteczka',
+    'policy': 'Cookie Policy'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -23,13 +50,15 @@ import { LayoutModule } from '@angular/cdk/layout';
     FooterComponent,
     MenuComponent,
     KontaktComponent,
-    OnasComponent
+    OnasComponent,
+    CiasteczkaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PopUpModule,
-    LayoutModule
+    LayoutModule,
+    NgcCookieConsentModule.forRoot(cookieConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
